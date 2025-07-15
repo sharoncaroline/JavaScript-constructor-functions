b49828849a04fe1e7871cc95c07145705ae4f932
sharon caroline <you@example.com>
2025-07-15 09:41:41 +0300

    Add constructor functions for Scooter, Driver, and PickupLocation

---
+++ index.js
@@ -0,0 +1,19 @@
+// Scooter constructor
+function Scooter(year, color, model) {
+  this.year = year;
+  this.color = color;
+  this.model = model;
+}
+
+// Driver constructor
+function Driver(name, age, experience) {
+  this.name = name;
+  this.age = age;
+  this.experience = experience;
+}
+
+// PickupLocation constructor
+function PickupLocation(address, city) {
+  this.address = address;
+  this.city = city;
+}
